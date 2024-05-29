86dd44deb7d8cb010c71e3880c230904941c42dc
Mihail Karmirkardiev <mirumir5436@yandex.ru>
2024-05-29 23:47:45 +0300

    Done. M1:Task4-6

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 
 # Create your views here.
@@ -12,6 +12,15 @@ author = {
     "email": "vasya@mail.ru"
 
 }
+
+items = [
+   {"id": 1, "name": "Кроссовки abibas" ,"quantity":5},
+   {"id": 2, "name": "Куртка кожаная" ,"quantity":2},
+   {"id": 5, "name": "Coca-cola 1 литр" ,"quantity":12},
+   {"id": 7, "name": "Картофель фри" ,"quantity":0},
+   {"id": 8, "name": "Кепка" ,"quantity":124},
+]
+
 
 def home(request):
     
@@ -29,4 +38,24 @@ def about(request):
     email: <b> {author['email']}</b><br>
 """
     return HttpResponse(text)
-    
+
+def get_item(request, item_id):
+    """По указанному id возвращаем имя элемента"""
+    for item in items:
+        if item['id'] == item_id:
+            result = f"""
+            <h2> Имя: {item["name"]}</h2>
+            <p> Количество: {item["quantity"]}</p>       
+            <p> <a href="/items"> Назад к списку товаров </a></p>
+            """
+            return HttpResponse(result)
+    return HttpResponseNotFound(f"Item with id={item_id} not found")
+
+def get_items(request):
+    result="<h1> Список товаров ,</h1><ol>"
+    for item in items:
+        result +=f"""
+        <li> <a href="/item/{item['id']}"> {item['name']}</li>
+        """
+    result += "</ol>"
+    return HttpResponse(result)
